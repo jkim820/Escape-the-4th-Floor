9f613abca5e798e57b9825b4d38a1be7a8c0adfb
John Kim <jkim82092@gmail.com>
2019-04-18 12:13:52 -0400

    Style modal and message

--- dist/lib/bundle.js
+++ dist/lib/bundle.js
@@ -93,43 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-document.addEventListener("DOMContentLoaded", () => {
-    // changeImage();
-}); 
 
-// function changeLeftImage() {
-//     // if (document.pic.src === './images/room1.png') {
-
-//     //     document.pic.src = 'room2.png';
-//     // }
-//     // else if (document.pic.src === './images/room2.png') {
-
-//     //     document.pic.src = 'room1.png';
-//     // }
-
-//     if (document.getElementById("pic").src.endsWith('room1.png') !== -1) {
-//         document.getElementById("pic").src = "./images/room3.png";
-//     } else if (window.document.pic.src.endsWith('room3.png') !== -1) {
-//         document.getElementById("pic").src = "./images/room1.png";
-//     }
-// }
-
-// function changeRightImage() {
-//     // if (document.pic.src === './images/room1.png') {
-
-//     //     document.pic.src = 'room2.png';
-//     // }
-//     // else if (document.pic.src === './images/room2.png') {
-
-//     //     document.pic.src = 'room1.png';
-//     // }
-
-//     if (document.getElementById("pic").src.endsWith('room1.png') !== -1) {
-//         document.getElementById("pic").src = "./images/room2.png";
-//     } else if (window.document.pic.src.endsWith('room2.png') !== -1) {
-//         document.getElementById("pic").src = "./images/room1.png";
-//     }
-// }
 
 /***/ })
 
